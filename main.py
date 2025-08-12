ff1f4ebb9be3de5cf23f97c9159bb653610ca358
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2025-08-13 07:17:00 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-import subprocess # instals libarys to run commands
-import subprocess
+import subprocess # installs libraries to run commands
 import json
 
 print("\n\n=====================")
 print("\n\nWelcome to cool inventory")
-subprocess.run('echo "" >> index.cool', shell=True) # runns echo "" >> index.cool command in linux
-subprocess.run('echo "" >> index.json', shell=True) # runns command in linux
+subprocess.run('echo "" >> index.cool', shell=True) # runs echo "" >> index.cool command in linux
+subprocess.run('echo "" >> index.json', shell=True) # runs command in linux
 
 data = {}
 
 ask = input("Is this your first time using cool? (Y/n) ")
-if ask == "y" or "Y":
-	item = input("What is your first fridge item? ")
-	amount = input("How much of " + item + " is there? ")
-	data[item] = int(amount)
-	with open("data.json", "w") as f:
-    		json.dump(data, f)
-
+if ask.lower() == "y":
+    item = input("What is your first fridge item? ")
+    amount = input("How much of " + item + " is there? ")
+    data[item] = int(amount)
+    with open("data.json", "w") as f:
+        json.dump(data, f)
 else:
-	with open("data.json", "w") as f:
-		json.load(data, f)
-
-	print("your current data is\n\n")
-	print(data)
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    print("your current data is\n\n")
+    print(data)
