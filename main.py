65d65b46a7f4cf967192aa26b4644fdd0f94bd47
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2025-08-13 06:57:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import subprocess # instals libarys to run commands
+import subprocess
+import json
 
 print("\n\n=====================")
 print("\n\nWelcome to cool inventory")
 subprocess.run('echo "" >> index.cool', shell=True) # runns echo "" >> index.cool command in linux
+subprocess.run('echo "" >> index.json', shell=True) # runns command in linux
+
+data = {}
+
+ask = input("Is this your first time using cool? (Y/n) ")
+if ask == "y" or "Y":
+	item = input("What is your first fridge item? ")
+	amount = input("How much of " + item + " is there? ")
+	data[item] = int(amount)
+	with open("data.json", "w") as f:
+    		json.dump(data, f)
+
+else:
+	with open("data.json", "w") as f:
+		json.load(data, f)
+
+	print("your current data is\n\n")
+	print(data)
